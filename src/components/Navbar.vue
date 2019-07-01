12d30ae3f0e2a9bc64edbc551eab5a3af5493875
oluwatunmise <oluwatunmise@crenettechlabs.com>
2019-07-01 19:11:58 +0100

    ui: added snackbars to show a quick notification after a new project is added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,11 @@
 <template>
     <nav>
+        <v-snackbar v-model="snackbar" :timeout="4000" top color="success">
+            <span>Awesome! you added a new project</span>
+            <v-btn flat color="white" @click="snackbar = false">
+                close
+            </v-btn>
+        </v-snackbar>
         <v-toolbar flat app>
             <v-toolbar-side-icon @click="drawer = !drawer" class="grey--text"></v-toolbar-side-icon>
             <!-- <v-toolbar-title class="text-uppercase grey--text">
@@ -35,7 +41,7 @@
                     </p>
                 </v-flex>
                 <v-flex class="mt-4 mb-3">
-                    <Popup />
+                    <Popup  @projectAdded="snackbar = true"/>
                 </v-flex>
             </v-layout>
            <v-list>
@@ -65,7 +71,8 @@ export default {
                 {icon:'dashboard', text: 'Dashbord', route: '/'},
                 {icon:'folder', text: 'My Projects', route: '/projects'},
                 {icon:'people', text: 'Team', route: '/teams'}
-            ]
+            ],
+            snackbar: false
         }
     }
 }
